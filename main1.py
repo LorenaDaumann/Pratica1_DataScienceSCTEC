71c54007d46eb77c1f9cd4290f54477753e8750b
Lorena Daumann <lorenadaumann@gmail.com>
2026-03-03 23:15:41 -0300

    fiz meu branch develop hehehe

--- main1.py
+++ main1.py
@@ -1,4 +1,14 @@
+# 1. Crie uma pasta local no seu pc e inicialize o git com git init
+# 2. Em seguida, crie um arquivo main.py e veja as modificações com git status
+# 3. utilize o git add e o add commit para salvar as modificações
+# 4. Crie um repositório remoto no github 
+# 5. conecte o repositório local com o comando git remote add origin <url do repositório> e sincronixe-os
+# 6. Crie uma branch de desenvolvimento com git checkout -b development
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+
+
+print("CRIEI UMA BRANCH EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
